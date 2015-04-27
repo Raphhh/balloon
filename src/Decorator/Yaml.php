491f58ed2b54e77698a5ab4b1e75b1d362bc61aa
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-04-27 20:48:09 +0200

    create Yaml decorator

---
+++ src/Decorator/Yaml.php
@@ -0,0 +1,65 @@
+<?php
+namespace Balloon\Decorator;
+
+use Balloon\Bridge\IFileReader;
+use Symfony\Component\Yaml\Dumper;
+use Symfony\Component\Yaml\Parser;
+
+/**
+ * Class Yaml
+ * @package Balloon\Decorator
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class Yaml implements IFileReader
+{
+    /**
+     * @var IFileReader
+     */
+    private $fileReader;
+
+    /**
+     * @var Parser
+     */
+    private $yamlParser;
+
+    /**
+     * @var Dumper
+     */
+    private $yamlDumper;
+    /**
+     * @var
+     */
+    private $dumpLevel;
+
+    /**
+     * @param IFileReader $fileReader
+     * @param Parser $yamlParser
+     * @param Dumper $yamlDumper
+     * @param $dumpLevel
+     */
+    public function __construct(IFileReader $fileReader, Parser $yamlParser, Dumper $yamlDumper, $dumpLevel = 0)
+    {
+        $this->fileReader = $fileReader;
+        $this->yamlParser = $yamlParser;
+        $this->yamlDumper = $yamlDumper;
+        $this->dumpLevel = $dumpLevel;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function read()
+    {
+        return $this->yamlParser->parse($this->fileReader->read());
+    }
+
+    /**
+     * @param mixed $data
+     * @param int $mode
+     * @return int
+     */
+    public function write($data, $mode = 0)
+    {
+        return $this->fileReader->write($this->yamlDumper->dump($data, $this->dumpLevel), $mode);
+    }
+}
---
+++ tests/Decorator/YamlTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Balloon\Decorator;
+
+use Balloon\Bridge\DummyFileReader;
+use Symfony\Component\Yaml\Dumper;
+use Symfony\Component\Yaml\Parser;
+
+/**
+ * Class YamlTest
+ * @package Balloon\Decorator
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class YamlTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRead()
+    {
+        $data = [['key1' => 'value1'], ['key2' => 'value2']];
+        $fileReader = new DummyFileReader();
+        $fileReader->write(json_encode($data));
+
+        $yaml = new Yaml($fileReader, new Parser(), new Dumper());
+        $result = $yaml->read();
+        $this->assertSame($data, $result);
+    }
+
+    public function testWrite()
+    {
+        $data = [['key1' => 'value1'], ['key2' => 'value2']];
+        $fileReader = new DummyFileReader();  ;
+
+        $yaml = new Yaml($fileReader, new Parser(), new Dumper(), 2);
+        $yaml->write($data);
+        $this->assertSame(
+            "-
+    key1: value1
+-
+    key2: value2
+",
+            $fileReader->read()
+        );
+    }
+}
